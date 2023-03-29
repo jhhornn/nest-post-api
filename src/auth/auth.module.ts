973eb90a83205a28000f130ee1f9c93cc44c6063
Awosise Oluwaseun <awosiseo@gmail.com>
2023-03-29 10:27:25 +0100

    feat(auth): imported and provided required jwt and passport module into auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config/dist';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthService, UsersRepository],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: configService.get('JWT_EXPIRES'),
+          },
+        };
+      },
+    }),
+  ],
+  providers: [AuthService, UsersRepository, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}

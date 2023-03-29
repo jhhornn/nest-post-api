2c0b1150c64d41ccf211db05b357d74d73946891
Awosise Oluwaseun <awosiseo@gmail.com>
2023-03-29 09:24:11 +0100

    feat(auth): injected repository into modules and services

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersRepository } from './users.repository';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, UsersRepository],
   controllers: [AuthController],
 })
 export class AuthModule {}
